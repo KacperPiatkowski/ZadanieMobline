bac8bc91a3e8c5f3e5aa63e6605db947eb7da432
KacperPiatkowski <k.piatkowski07@gmail.com>
2024-01-23 14:46:26 +0100

    zmienione

--- app/src/main/java/com/example/zadaniemobline/infouztykownik.kt
+++ app/src/main/java/com/example/zadaniemobline/infouztykownik.kt
@@ -1,17 +1,20 @@
 package com.example.zadaniemobline
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.navigation.NavigationView
 
 class infouztykownik : AppCompatActivity() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_infouztykownik)
@@ -24,6 +27,10 @@ class infouztykownik : AppCompatActivity() {
 
         findViewById<Button>(R.id.btnwyjdz).setOnClickListener {
             navigationView.visibility = View.GONE
+        }
+
+        findViewById<Button>(R.id.wyloguj).setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java).putExtra("Przerzucanie", tablica))
         }
 
         findViewById<ImageView>(R.id.ikonka).setOnClickListener {
